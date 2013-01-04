1def6ec84c6f943d04690e8e2d5dccd94a32c736
Meng Feng <mengfeng0904@gmail.com>
2013-01-04 21:04:44 +0800

    Navigation Controller changed. ZPics Skeleton View Controller is added

---
+++ ZPicDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZPicDetailsViewController.h
+//  Zthings
+//
+//  Created by Feng on 4/1/13.
+//  Copyright (c) 2013 Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZPicDetailsViewController : UIViewController
+
+@end
---
+++ Zthings/ZPicDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZPicDetailViewController.h
+//  Zthings
+//
+//  Created by Feng on 4/1/13.
+//  Copyright (c) 2013 Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZPicDetailViewController : UITableViewController
+
+@end
